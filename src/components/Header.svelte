b84f56d999dffec2f574f131db61094b4c30f401
zlima <endercraft68@gmail.com>
2023-12-31 19:06:13 -0300

    Complete intro section in the Main component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -24,10 +24,12 @@
   <a
     href="mailto:zacharylima2021@gmail.com"
     target="_blank"
-    class="redShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
-    <div class="absolute top-0 right-full w-full h-full bg-[#F9013F] opacity-20 group-hover:translate-x-full z-0 duration-200">
-
-    </div>
+    class="redShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+  >
+    <div 
+      class="absolute top-0 right-full w-full h-full bg-[#F9013F] opacity-20 
+      group-hover:translate-x-full z-0 duration-200"
+    />
     <h4 class="relative z-9">Get in touch</h4>
   </a>
 </header>
---
+++ src/components/Main.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-[#F9013F]">Zachary</span> Lima <br/>Full Stack
+        <span class="poppins text-[#F9013F]">Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My <span class="text-[#F9013F]">favorite tech</span> includes C++, Python, Java, JavaScript 
+        (SvelteKit, React, Node.js), TailwindCSS, and Git
+      </p>
+      <a
+        href="mailto:zacharylima2021@gmail.com"
+        target="_blank"
+        class="redShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins 
+        relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950
+        cursor-pointer"
+      >
+        <div 
+          class="absolute top-0 right-full w-full h-full bg-[#F9013F] opacity-20 
+          group-hover:translate-x-full z-0 duration-200"
+        />
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img src={"images/profile-pic.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
+    </div>
+  </section>
+</main>
