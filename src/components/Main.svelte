491b69038a4d9bef1f720c8040771497f5b0e945
zlima <endercraft68@gmail.com>
2023-12-31 19:27:17 -0300

    Add projects section and steps component

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,5 +1,11 @@
 <script>
+  import Step from './Step.svelte';
 
+  let steps = [
+    {name: 'Video Game Price Scraper', icon: 'fa-solid fa-gamepad'},
+    {name: 'Processor Simulator', icon: 'fa-solid fa-microchip'},
+    {name: 'Cache Simulator', icon: 'fa-solid fa-memory'},
+  ]
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -28,7 +34,42 @@
       </a>
     </div>
     <div class="relative shadow-2xl grid place-items-center">
-      <img src={"images/profile-pic.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
+      <img 
+        src={"images/profile-pic.png"} 
+        alt="Profile image" 
+        class="object-cover z-[2] max-h-[70vh]"/>
     </div>
+  </section>
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">
+        A few of my creative endeavors.
+      </h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Curious to <span class="poppins text-[#F9013F]">see</span> my
+        work?
+      </h3>
+    </div>
+    <a 
+        href=""
+        target="_blank"
+        class="mx-auto px-4 py-2 rounded-md border
+        border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 
+        -mt-10 hover:border-[#F9013F] duration-200">
+          <i class="fa-regular fa-circle-play"></i>
+          <p>Watch the video</p>
+      </a>
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+        <Step step={steps[0]}>
+
+        </Step>
+        <Step step={steps[1]}>
+
+        </Step>
+        <Step step={steps[2]}>
+
+        </Step>
+      </div>
   </section>
 </main>
