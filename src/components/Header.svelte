f402d5e50ed72773a65fbf32830207f0030064d5
zlima <endercraft68@gmail.com>
2023-12-31 18:43:21 -0300

    Add title to app.html and update header and layout styles

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,22 +2,32 @@
   export let y;
 
   let tabs = [
+    { name: 'About', link: '#about'},
+    { name: 'Experience', link: '#experience'},
     { name: 'Projects', link: '#projects'},
-    { name: 'About me', link: '#about'},
   ];
 </script>
 
 <header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid ' + (
-  y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
+  y > 0 ? ' py-4 bg-slate-950 border-[#F9013F]' : ' py-6 bg-transparent border-transparent'
 )}>
   <h1 class="font-medium">
     <b class="font-bold poppins">Zachary</b> Lima
   </h1>
   <div class="sm:flex items-center gap-4 hidden">
     {#each tabs as tab, index}
-      <a href={tab.link} class="duration-200 hover:text-violet-400">
+      <a href={tab.link} class="duration-200 hover:text-[#F9013F]">
         <p>{tab.name}</p>
       </a>
     {/each}
   </div>
+  <a
+    href="mailto:zacharylima2021@gmail.com"
+    target="_blank"
+    class="redShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <div class="absolute top-0 right-full w-full h-full bg-[#F9013F] opacity-20 group-hover:translate-x-full z-0 duration-200">
+
+    </div>
+    <h4 class="relative z-9">Get in touch</h4>
+  </a>
 </header>
