5fc7dba066194541f28029e5c4e219c528f6bc90
zlima <endercraft68@gmail.com>
2023-12-31 18:23:19 -0300

    Update background and particle colors

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,15 +7,15 @@
   ];
 </script>
 
-<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (
-  y > 0 ? ' py-4 bg-[#782F40] border-[#CEB888]' : ' py-6 bg-transparent border-transparent'
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid ' + (
+  y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
 )}>
   <h1 class="font-medium">
     <b class="font-bold poppins">Zachary</b> Lima
   </h1>
   <div class="sm:flex items-center gap-4 hidden">
     {#each tabs as tab, index}
-      <a href={tab.link} class="duration-200 hover:text-[#CEB888]">
+      <a href={tab.link} class="duration-200 hover:text-violet-400">
         <p>{tab.name}</p>
       </a>
     {/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
   <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
     y > 0 ? ' opacity-full pointer-events-auto': ' pointer-events-none opacity-0'
   )}>
-    <button on:click(goTop) class="ml-auto rounded-full bg-[#782F40] text-[#CEB888]-400 px-3 sm:px-4 hover:bg-[#782F40] cursor-pointer">
+    <button on:click(goTop) class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800] cursor-pointer">
       <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
     </button>
   </div>
