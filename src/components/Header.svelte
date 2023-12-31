1144f8fd06eaa57f025b2f7b3469c73db8b82780
zlima <endercraft68@gmail.com>
2023-12-31 15:25:25 -0300

    Create header components and layout

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let y;
+
+  let tabs = [
+    { name: 'Projects', link: '#projects'},
+    { name: 'About me', link: '#about'},
+  ];
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (
+  y > 0 ? ' py-4 bg-[#782F40] border-[#CEB888]' : ' py-6 bg-transparent border-transparent'
+)}>
+  <h1 class="font-medium">
+    <b class="font-bold poppins">Zachary</b> Lima
+  </h1>
+  <div class="sm:flex items-center gap-4 hidden">
+    {#each tabs as tab, index}
+      <a href={tab.link} class="duration-200 hover:text-[#CEB888]">
+        <p>{tab.name}</p>
+      </a>
+    {/each}
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
   import "../app.css";
+  import Footer from '../components/Footer.svelte';
+  import Header from '../components/Header.svelte';
+
+  let y;
+  let innerHeight = 0;
+  let innerWidth = 0;
+
+  function goTop() {
+    document.body.scrollIntoView();
+  }
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+  <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
+    y > 0 ? ' opacity-full pointer-events-auto': ' pointer-events-none opacity-0'
+  )}>
+    <button on:click(goTop) class="ml-auto rounded-full bg-[#782F40] text-[#CEB888]-400 px-3 sm:px-4 hover:bg-[#782F40] cursor-pointer">
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
+    </button>
+  </div>
+  <Header y={y}/>
+  <slot />
+  <Footer/>
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
