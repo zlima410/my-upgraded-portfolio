8f02408a1ec8721ff100365b32a76f1401c9231b
zlima <endercraft68@gmail.com>
2023-12-31 20:29:05 -0300

    Create details and layout for about me and projects sections

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,9 +2,8 @@
   export let y;
 
   let tabs = [
-    { name: 'About', link: '#about'},
-    { name: 'Experience', link: '#experience'},
     { name: 'Projects', link: '#projects'},
+    { name: 'About Me', link: '#about'},
   ];
 </script>
 
@@ -14,7 +13,7 @@
   <h1 class="font-medium">
     <b class="font-bold poppins">Zachary</b> Lima
   </h1>
-  <div class="sm:flex items-center gap-4 hidden">
+  <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
     {#each tabs as tab, index}
       <a href={tab.link} class="duration-200 hover:text-[#F9013F]">
         <p>{tab.name}</p>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,8 +3,14 @@
 
   let steps = [
     {name: 'Video Game Price Scraper', icon: 'fa-solid fa-gamepad'},
-    {name: 'Processor Simulator', icon: 'fa-solid fa-microchip'},
-    {name: 'Cache Simulator', icon: 'fa-solid fa-memory'},
+    {name: 'LC3100 Processor Simulator', icon: 'fa-solid fa-microchip'},
+    {name: 'LC3100 Cache Simulator', icon: 'fa-solid fa-memory'},
+  ];
+
+  let benefits = [
+    {name: 'a 3.35 GPA student', description: 'Crafting a 3.35 GPA as a Computer Science student at Florida State University was a testament to my disciplined approach to learning. Through unwavering commitment, I diligently balanced theoretical coursework with hands-on projects, leveraging resources like coding boot camps and peer study groups.  This GPA embodies not just academic success but also the dedication and perseverance integral to my journey as a computer scientist.'},
+    {name: 'a problem solver', description: "My strength in problem-solving spans various domains, notably in computer science, where my analytical abilities truly thrive. I approach challenges methodically, adapting seamlessly to dissect complex issues and strategize solutions effectively. Whether it's debugging intricate code or tackling real-world problems, my talent lies in breaking down complexities into manageable parts, allowing me to innovate solutions across diverse fields."},
+    {name: 'an excellent communicator', description: 'My communication skills allow me to articulate complex technical concepts with clarity and precision. Beyond technical language, I excel in breaking down intricate ideas into easily understandable terms, fostering collaboration among diverse teams. Through active listening, empathy, and effective expression, I not only enhance team dynamics within computer science but also navigate various interpersonal and professional scenarios with ease.'},
   ]
 </script>
 
@@ -16,8 +22,8 @@
         <span class="poppins text-[#F9013F]">Developer</span>
       </h2>
       <p class="text-base sm:text-lg md:text-xl">
-        My <span class="text-[#F9013F]">favorite tech</span> includes C++, Python, Java, JavaScript 
-        (SvelteKit, React, Node.js), TailwindCSS, and Git
+        My <span class="text-[#F9013F]">favorite tech</span> includes C, C++, Python, Java, JavaScript 
+        (SvelteKit, Node.js), TailwindCSS, and Git
       </p>
       <a
         href="mailto:zacharylima2021@gmail.com"
@@ -51,7 +57,7 @@
         work?
       </h3>
     </div>
-    <a 
+    <!-- <a 
         href=""
         target="_blank"
         class="mx-auto px-4 py-2 rounded-md border
@@ -59,17 +65,58 @@
         -mt-10 hover:border-[#F9013F] duration-200">
           <i class="fa-regular fa-circle-play"></i>
           <p>Watch the video</p>
-      </a>
+      </a> -->
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
         <Step step={steps[0]}>
-
+          <p>A webscraper to find changes in video game prices using 
+            <strong class="text-[#F9013F]">Python! </strong>BeautifulSoup is used for webscraping
+            and Pandas is used to export data to Excel.
+          </p>
         </Step>
         <Step step={steps[1]}>
-
+          <p>A processor simulator that simulates the execution of the resulting machine code 
+            representation of a program using <strong class="text-[#F9013F]">C!</strong>
+            The LC3100 is an 8-register, 32-bit computer. All addresses are word-addresses. 
+            The LC3100 has 65536 words of memory.
+          </p>
         </Step>
         <Step step={steps[2]}>
-
+          <p>As the processor simulator executes a program, it will access instructions and data. 
+            These accesses will be serviced by this cache simulator, which will transfer data to/from memory as needed. 
+            This simulator was made with <strong class="text-[#F9013F]">C!</strong>
+          </p>
         </Step>
+      </div>
+  </section>
+
+  <section 
+    id="about" 
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col 
+    gap-16 sm:gap-20 md:gap-24 relative">
+      <div 
+        class="flex flex-col gap-2 text-center relative before:absolute before:top-0
+        before:left-0 before:w-2/3 before:h-1.5 before:bg-[#F9013F] after:absolute 
+        after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-[#F9013F] py-4">
+          <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+          <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+            A bit <span class="poppins text-[#F9013F]">about</span> me.
+          </h3>
+      </div>
+      <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+      <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+        {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+            0{index+1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+        {/each}
       </div>
   </section>
 </main>
