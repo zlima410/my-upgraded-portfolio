6635f2457e54ca6712a57c1e5f6ea470c86e12d2
zlima <endercraft68@gmail.com>
2023-12-31 21:20:18 -0300

    Complete the Main component after adding chacteristics table

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer 
+  class="py-20 sm:py-32 bg-black border-t border-solid border-[#F9013F]
+  flex flex-col gap-4 sm:gap-8 justify-center items-center">
+
+</footer>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -118,5 +118,72 @@
         </div>
         {/each}
       </div>
+      <h5 
+        class="text-2xl sm:text-3xl font-semibold text-center poppins">
+        The <span class="poppins text-[#F9013F]">Complete</span> Package
+      </h5>
+      <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
+        <table class="bg-white text-slate-700 rounded text-center">
+          <thead class="border-b border-solid border-slate-200">
+            <tr class="">
+              <th />
+              <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+              <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+              <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+              <th class="whitespace-nowrap bg-[#F9013F] text-white p-4 px-8">Me</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-solid border-slate-200">
+              <td 
+                class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold 
+                text-sm whitespace-nowrap">
+                  Dedication
+              </td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-green-500"></i></td>
+            </tr>
+            <tr class="border-b border-solid border-slate-200">
+              <td 
+                class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold 
+                text-sm whitespace-nowrap">
+                  Critical Thought
+              </td>
+              <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-green-500"></i></td>
+            </tr>
+            <tr class="border-b border-solid border-slate-200">
+              <td 
+                class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold 
+                text-sm whitespace-nowrap">
+                  Interpersonal Skills
+              </td>
+              <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-green-500"></i></td>
+            </tr>
+            <tr class="border-b border-solid border-slate-200">
+              <td 
+                class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold 
+                text-sm whitespace-nowrap">
+                  Programming Ability
+              </td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-slate-500"></i></td>
+              <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+              <td><i class="fa-solid fa-check text-green-500"></i></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+        <p>Scroll to see more &rarr;</p>
+      </div>
+      <p class="mx-auto">So why not invest?</p>
   </section>
 </main>
